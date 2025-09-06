5290e4ac8e0b9d01cf9d5422f68da766f8eebfef
franpass87 <francesco.passeri@gmail.com>
2025-09-06 12:59:54 +0200

    Add activation version requirement test

---
+++ tests/ActivationVersionCheckTest.php
@@ -0,0 +1,47 @@
+<?php
+if (!function_exists('get_bloginfo')) {
+    function get_bloginfo($show = '') {
+        return '4.0';
+    }
+}
+if (!function_exists('deactivate_plugins')) {
+    function deactivate_plugins($plugin) {
+        $GLOBALS['deactivated_plugin'] = $plugin;
+    }
+}
+if (!function_exists('plugin_basename')) {
+    function plugin_basename($file) {
+        return $file;
+    }
+}
+if (!function_exists('wp_die')) {
+    function wp_die($message) {
+        throw new Exception($message);
+    }
+}
+if (!function_exists('__')) {
+    function __($text, $domain = null) {
+        return $text;
+    }
+}
+
+use PHPUnit\Framework\TestCase;
+
+class ActivationVersionCheckTest extends TestCase
+{
+    public function test_activation_blocks_on_low_wp_version()
+    {
+        require_once __DIR__ . '/../FP-Hotel-In-Cloud-Monitoraggio-Conversioni.php';
+
+        try {
+            \FpHic\hic_activate(false);
+            $this->fail('Activation should have been blocked.');
+        } catch (Exception $e) {
+            $this->assertStringContainsString('Richiede almeno PHP', $e->getMessage());
+            $this->assertEquals(
+                realpath(__DIR__ . '/../FP-Hotel-In-Cloud-Monitoraggio-Conversioni.php'),
+                $GLOBALS['deactivated_plugin'] ?? null
+            );
+        }
+    }
+}
